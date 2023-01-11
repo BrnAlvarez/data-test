f496aca1ba4603ac68ff490de9e07ec60cbe2eac
BrnAlvarez <48591745+BrnAlvarez@users.noreply.github.com>
2023-01-11 16:16:26 -0300

    data-city-show.js
    asdasdasdas

---
+++ data-city-show.js
@@ -0,0 +1,10 @@
+[
+    {id: 1, name: 'Región del Maule'},
+    {id: 2, name: 'Talca'},
+    {id: 3, name: 'Linares'},
+    {id: 5, name: 'Constitución'},
+    {id: 6, name: 'Iloca/Duao'},
+    {id: 7, name: 'San Clemente'},
+    {id: 8, name: 'Armarillo'},
+    {id: 9, name: 'Vilches'}
+]
